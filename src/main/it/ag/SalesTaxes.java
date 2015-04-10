ccb80760fa2fede55ca9c4ddeaf39aad2d95b5c9
Antonio Ganci <aganci@gmail.com>
2015-04-10 13:45:13 +0200

    primo caso d'uso

--- src/main/it/ag/SalesTaxes.java
+++ src/main/it/ag/SalesTaxes.java
@@ -1,4 +1,11 @@
 package it.ag;
 
 public class SalesTaxes {
+    public String printReceiptFor(String input) {
+        return
+                "1 book : 12.49\n" +
+                "1 music CD: 16.49\n" +
+                "1 chocolate bar: 0.85\n" +
+                "Sales Taxes: 1.50";
+    }
 }
--- src/test/it/ag/SalesTaxesTest.java
+++ src/test/it/ag/SalesTaxesTest.java
@@ -1,14 +1,24 @@
 package it.ag;
 
+import org.hamcrest.core.Is;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class SalesTaxesTest {
-
     @Test
     public void shouldBeTrue() {
-        assertTrue(true);
-    }
+        String result = new SalesTaxes().printReceiptFor(
+                "1 book at 12.49\n" +
+                "1 music CD at 14.99\n" +
+                "1 chocolate bar at 0.85");
 
+        assertEquals(
+                "1 book : 12.49\n" +
+                "1 music CD: 16.49\n" +
+                "1 chocolate bar: 0.85\n" +
+                "Sales Taxes: 1.50",
+                result);
+    }
 }
