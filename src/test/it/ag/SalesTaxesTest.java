41088bd0d445cd4d2e9fc8c255fd92c6597512f7
Antonio Ganci <aganci@gmail.com>
2015-04-10 12:24:04 +0200

    progetto vuoto

---
+++ src/test/it/ag/SalesTaxesTest.java
@@ -0,0 +1,14 @@
+package it.ag;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SalesTaxesTest {
+
+    @Test
+    public void shouldBeTrue() {
+        assertTrue(true);
+    }
+
+}
